c9f53de2ce93fa7b2040db29e380fce566b8e371
GoBig87 <39137894+GoBig87@users.noreply.github.com>
2018-06-17 23:54:52 -0700

    Adding some cdef class creations

--- stripe_ios.pyx
+++ stripe_ios.pyx
@@ -6,11 +6,18 @@ cdef extern from "stripe_ios.h":
     strip_wrapper_t stripe_wrapper_init()
     const char stripe_get_token(strip_wrapper_t stripe, int cardNumber, int expMonth, int expYear,int cvc)
 
+cdef class _Stripe:
+    cdef strip_wrapper_t stripe
+
+    def __cinit__(self):
+        self.stripe = NULL
+
 class StripeWrapper():
 
-    def __cinit__(self,**kwargs):
-        super(StripeWrapper, self).__cinit__(**kwargs)
+    def __init__(self,**kwargs):
+        super(StripeWrapper, self).__init__(**kwargs)
 
     def getToken(self,cardNumber,expMonth,expYear,cvc):
-        stripe = stripe_wrapper_init()
-        self.token = stripe_get_token(stripe,cardNumber,expMonth,expYear,cvc)
+        cdef _Stripe storage = <_Stripe>self._storage
+        storage.stripe = stripe_wrapper_init()
+        self.token = stripe_get_token(storage.stripe,cardNumber,expMonth,expYear,cvc)
