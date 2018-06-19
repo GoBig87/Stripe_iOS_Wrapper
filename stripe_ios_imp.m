6d3f17c47a01c9d2373a1c66db0a0440b90b98dd
GoBig87 <39137894+GoBig87@users.noreply.github.com>
2018-06-19 10:13:39 -0700

    Need to declare NSString

--- stripe_ios_imp.m
+++ stripe_ios_imp.m
@@ -16,14 +16,14 @@ StripWrapper::StripWrapper(){
 
 void StripWrapper::retrieveToken(const char* cardNumber, int expMonth, int expYear, const char* cvc) {
 
-    cardNumberNS = [NSString stringWithUTF8String:cardNumber];
-    cvcNS = [NSString stringWithUTF8String:cvc];
+    NSString NScardNumber = [NSString stringWithUTF8String:cardNumber];
+    NSString NScvc = [NSString stringWithUTF8String:cvc];
 
     STPCardParams *cardParams = [[STPCardParams alloc] init];
-    cardParams.number = cardNumberNS;
+    cardParams.number = NScardNumber;
     cardParams.expMonth = expMonth;
     cardParams.expYear = expYear;
-    cardParams.cvc = cvcNS;
+    cardParams.cvc = NScvc;
 
     [[STPAPIClient sharedClient] createTokenWithCard:cardParams completion:^(STPToken *token, NSError *error) {
         if (token == nil || error != nil) {
