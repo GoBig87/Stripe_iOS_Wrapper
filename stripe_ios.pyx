97eea50184638d95ab708c2d676240bf11cfca47
GoBig87 <39137894+GoBig87@users.noreply.github.com>
2018-06-21 09:34:55 -0700

    Changing to char*

--- stripe_ios.pyx
+++ stripe_ios.pyx
@@ -4,7 +4,7 @@ __all__ = ['StripeWrapper']
 cdef extern from "stripe_ios_imp.h":
     ctypedef void *strip_wrapper_t
     strip_wrapper_t stripe_wrapper_init()
-    const char* stripe_get_token(strip_wrapper_t stripe,const char* myKey, const char* cardNumber, int expMonth, int expYear,const char* cvc)
+    char* stripe_get_token(strip_wrapper_t stripe,char* myKey,char* cardNumber, int expMonth, int expYear,char* cvc)
 
 cdef class _Stripe:
     cdef strip_wrapper_t stripe
@@ -21,13 +21,13 @@ class StripeWrapper():
         cdef _Stripe storage = <_Stripe>self._storage
 
         cdef bytes myKey_bytes = myKey.encode('UTF-8')
-        cdef const char* myKey_string = myKey_bytes
+        cdef char* myKey_string = myKey_bytes
         cdef bytes cardNumber_bytes = cardNumber.encode('UTF-8')
-        cdef const char* cardNumber_string = cardNumber_bytes
+        cdef char* cardNumber_string = cardNumber_bytes
         cdef bytes cvc_bytes = cvc.encode('UTF-8')
-        cdef const char* cvc_string = cvc_bytes
+        cdef char* cvc_string = cvc_bytes
 
         storage.stripe = stripe_wrapper_init()
-        cdef const char* c_string_token =  stripe_get_token(storage.stripe,myKey_string,cardNumber_string,expMonth,expYear,cvc_string)
+        cdef char* c_string_token =  stripe_get_token(storage.stripe,myKey_string,cardNumber_string,expMonth,expYear,cvc_string)
         cdef bytes py_string_token = c_string_token
         return py_string_token
