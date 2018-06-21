5cc30ad90fe5286bf88a7e372b5e169c1759b85f
GoBig87 <39137894+GoBig87@users.noreply.github.com>
2018-06-21 10:36:16 -0700

    edit

--- stripe_ios.pyx
+++ stripe_ios.pyx
@@ -21,11 +21,11 @@ class StripeWrapper():
         cdef _Stripe storage = <_Stripe>self._storage
 
         cdef bytes myKey_bytes = myKey.encode('UTF-8')
-        cdef char* myKey_string = myKey_bytes
+        cdef const char* myKey_string = myKey_bytes
         cdef bytes cardNumber_bytes = cardNumber.encode('UTF-8')
-        cdef char* cardNumber_string = cardNumber_bytes
+        cdef const char* cardNumber_string = cardNumber_bytes
         cdef bytes cvc_bytes = cvc.encode('UTF-8')
-        cdef char* cvc_string = cvc_bytes
+        cdef const char* cvc_string = cvc_bytes
 
         storage.stripe = stripe_wrapper_init()
         cdef const char* c_string_token =  stripe_get_token(storage.stripe,myKey_bytes,cardNumber_string,expMonth,expYear,cvc_string)
