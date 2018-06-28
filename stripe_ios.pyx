5851661cac870779758d67c32a1273903eca649c
GoBig87 <nobody5390@yahoo.com>
2018-06-27 22:34:21 -0700

    debug

--- stripe_ios.pyx
+++ stripe_ios.pyx
@@ -2,27 +2,19 @@ STUFF = "Hi"
 __all__ = ['StripeWrapper']
 
 cdef extern from "stripe_ios_imp.h":
-    ctypedef void *stripe_wrapper_t
     ctypedef void (*tokenfunc)(char *name, void *user_data)
-    stripe_wrapper_t stripe_wrapper_init()
-    void stripe_get_token(stripe_wrapper_t stripe, char* myKey, char* cardNumber, int expMonth, int expYear, char* cvc,tokenfunc callback_func,void* user_data)
+    void retrieveToken(char* myKey, char* cardNumber, int expMonth, int expYear, char* cvc,tokenfunc callback_func,void* user_data)
 
 cdef void callback(char *name, void *f):
     (<object>f)(name.decode('utf-8'))
 
-cdef class _Stripe:
-    cdef stripe_wrapper_t stripe
-
-    def __cinit__(self):
-        self.stripe = NULL
-
 class StripeWrapper():
 
     def __init__(self,**kwargs):
-        self._storage = _Stripe()
+        foo = 'bar'
+        pass
 
     def getToken(self,util,myKey,cardNumber,expMonth,expYear,cvc):
-        cdef _Stripe storage = <_Stripe>self._storage
 
         cdef bytes myKey_bytes = myKey.encode('utf-8')
         cdef char* myKey_string = myKey_bytes
@@ -39,8 +31,7 @@ class StripeWrapper():
         print cvc_string
         cdef char* token_callback
 
-        storage.stripe = stripe_wrapper_init()
-        stripe_get_token(storage.stripe,myKey_bytes,cardNumber_bytes,expMonth,expYear,cvc_bytes, callback, <void*>token_callback)
+        retrieveToken(myKey_bytes,cardNumber_bytes,expMonth,expYear,cvc_bytes, callback, <void*>token_callback)
         print token_callback
         util.token = token_callback.decode('UTF-8')
         print 'Debug 1'
