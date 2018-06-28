f58c97119a740b6996d15832d106692d6650f0f3
GoBig87 <nobody5390@yahoo.com>
2018-06-27 23:24:36 -0700

    debuyg

--- stripe_ios_imp.h
+++ stripe_ios_imp.h
@@ -1,2 +1,2 @@
-typedef void (*tokenfunc)(char *name, void *user_data);
+typedef void (*tokenfunc)(const char *name, void *user_data);
 void retrieveToken(char* myKey, char* cardNumber, int expMonth, int expYear, char* cvc,tokenfunc user_func, void *user_data);
