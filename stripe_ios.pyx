62bf8c1338ff1330a31c07f4f900f877f329ffbe
GoBig87 <nobody5390@yahoo.com>
2018-06-27 22:46:28 -0700

    debug

--- stripe_ios.pyx
+++ stripe_ios.pyx
@@ -8,33 +8,27 @@ cdef extern from "stripe_ios_imp.h":
 cdef void callback(char *name, void *f):
     (<object>f)(name.decode('utf-8'))
 
-class StripeWrapper():
+def getToken(self,util,myKey,cardNumber,expMonth,expYear,cvc):
 
-    def __init__(self,**kwargs):
-        foo = 'bar'
-        pass
+    cdef bytes myKey_bytes = myKey.encode('utf-8')
+    cdef char* myKey_string = myKey_bytes
+    cdef bytes cardNumber_bytes = cardNumber.encode('utf-8')
+    cdef char* cardNumber_string = cardNumber_bytes
+    cdef bytes cvc_bytes = cvc.encode('utf-8')
+    cdef char* cvc_string = cvc_bytes
 
-    def getToken(self,util,myKey,cardNumber,expMonth,expYear,cvc):
+    print myKey_bytes
+    print cardNumber_bytes
+    print cvc_bytes
+    print myKey_string
+    print cardNumber_string
+    print cvc_string
+    cdef char* token_callback
 
-        cdef bytes myKey_bytes = myKey.encode('utf-8')
-        cdef char* myKey_string = myKey_bytes
-        cdef bytes cardNumber_bytes = cardNumber.encode('utf-8')
-        cdef char* cardNumber_string = cardNumber_bytes
-        cdef bytes cvc_bytes = cvc.encode('utf-8')
-        cdef char* cvc_string = cvc_bytes
-
-        print myKey_bytes
-        print cardNumber_bytes
-        print cvc_bytes
-        print myKey_string
-        print cardNumber_string
-        print cvc_string
-        cdef char* token_callback
-
-        retrieveToken(callback, myKey_bytes, cardNumber_bytes, expMonth, expYear, cvc_bytes, <void*>token_callback)
-        print token_callback
-        util.token = token_callback.decode('UTF-8')
-        print 'Debug 1'
-        print util.token
+    retrieveToken( myKey_bytes, cardNumber_bytes, expMonth, expYear, cvc_bytes, callback, <void*>token_callback)
+    print token_callback
+    util.token = token_callback.decode('UTF-8')
+    print 'Debug 1'
+    print util.token
 
 
