9c95c4af11210553711fc51acd6c166099c7f268
GoBig87 <nobody5390@yahoo.com>
2018-06-27 23:03:42 -0700

    debug

--- stripe_ios_imp.h
+++ stripe_ios_imp.h
@@ -1,3 +1,3 @@
 typedef void *tokenfunc(char *name, void *user_data);
 
-void retrieveToken(char* myKey, char* cardNumber, int expMonth, int expYear, char* cvc,tokenfunc callback_func, void *user_data);
+void retrieveToken(char* myKey, char* cardNumber, int expMonth, int expYear, char* cvc,tokenfunc user_func, void *user_data);
