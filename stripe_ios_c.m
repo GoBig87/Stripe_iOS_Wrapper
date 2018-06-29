f7812603fd922989775a9858288ac28571eaa908
GoBig87 <nobody5390@yahoo.com>
2018-06-28 23:31:51 -0700

    debug

--- stripe_ios_c.m
+++ stripe_ios_c.m
@@ -3,5 +3,5 @@
 
 void retrieveToken(char* myKey, char* cardNumber, int expMonth, int expYear, char* cvc,tokenfunc user_func, void *user_data){
     retToken* retrieveToken = [[retToken alloc] init];
-    [retrieveToken retrieveTokenObjC:myKey andcardNumber:cardNumber andexpMonth:expMonth andexpYear:expYear anduser_func:user_func anduser_data:user_data];
+    [retrieveToken retrieveTokenObjC:myKey andcardNumber:cardNumber andexpMonth:expMonth andexpYear:expYear andcvc:cvc anduser_func:user_func anduser_data:user_data];
 }
