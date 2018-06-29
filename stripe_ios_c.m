7da1bb84baa02d427150bbe5a0072bdbe3fc60f9
GoBig87 <nobody5390@yahoo.com>
2018-06-28 22:36:17 -0700

    debug

--- stripe_ios_c.m
+++ stripe_ios_c.m
@@ -2,5 +2,6 @@
 #include "stripe_ios_imp.h"
 
 void retrieveToken(char* myKey, char* cardNumber, int expMonth, int expYear, char* cvc,tokenfunc user_func, void *user_data){
+    retToken retrieveToken;
     retrieveTokenObjC(myKey,cardNumber,expMonth,expYear,cvc,tokenfunc,user_data);
 }
