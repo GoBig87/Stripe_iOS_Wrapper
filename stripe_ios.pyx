a580036612587233223d73d021fa3eb91b0757a4
GoBig87 <nobody5390@yahoo.com>
2018-06-29 13:45:00 -0700

    debug

--- stripe_ios.pyx
+++ stripe_ios.pyx
@@ -5,38 +5,24 @@ cdef extern from "stripe_ios_c.h":
     ctypedef void (*tokenfunc)(const char *name, void *user_data)
     void retrieveToken(char* myKey, char* cardNumber, int expMonth, int expYear, char* cvc,tokenfunc user_func, void *user_data)
 
-class StripeWrapper():
+def getToken(tokenCallback,myKey,cardNumber,expMonth,expYear,cvc):
 
-    def __init__(self,**kwargs):
-        foo = 'bar'
-        pass
+    cdef bytes myKey_bytes = myKey.encode('utf-8')
+    cdef char* myKey_string = myKey_bytes
+    cdef bytes cardNumber_bytes = cardNumber.encode('utf-8')
+    cdef char* cardNumber_string = cardNumber_bytes
+    cdef bytes cvc_bytes = cvc.encode('utf-8')
+    cdef char* cvc_string = cvc_bytes
 
-    def getToken(self,tokenCallback,myKey,cardNumber,expMonth,expYear,cvc):
+    print myKey_bytes
+    print cardNumber_bytes
+    print cvc_bytes
+    print myKey_string
+    print cardNumber_string
+    print cvc_string
+    retrieveToken(myKey_bytes, cardNumber_bytes, expMonth, expYear, cvc_bytes, callback, <void*>tokenCallback)
+    print 'Debug 1'
 
-        cdef bytes myKey_bytes = myKey.encode('utf-8')
-        cdef char* myKey_string = myKey_bytes
-        cdef bytes cardNumber_bytes = cardNumber.encode('utf-8')
-        cdef char* cardNumber_string = cardNumber_bytes
-        cdef bytes cvc_bytes = cvc.encode('utf-8')
-        cdef char* cvc_string = cvc_bytes
-
-        print myKey_bytes
-        print cardNumber_bytes
-        print cvc_bytes
-        print myKey_string
-        print cardNumber_string
-        print cvc_string
-
-        cdef Callback cb
-
-        retrieveToken(myKey_bytes, cardNumber_bytes, expMonth, expYear, cvc_bytes, cb.callback, <void*>tokenCallback)
-        print 'Debug 1'
-
-cdef class Callback:
-    cdef void* tokenCallback
-    cdef const char* name
-
-    @staticmethod
-    cdef callback(const char* name, void *tokenCallback):
-        print 'token debug'
-        (< object > tokenCallback)(name.decode('utf-8'))
+cdef void callback(const char * name, void * tokenCallback):
+    print 'callback debug'
+    (< object > tokenCallback)(name.decode('utf-8'))
