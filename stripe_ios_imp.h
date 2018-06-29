84b430c8af46bf530e5ab7b6ef2490e5cec2eab7
GoBig87 <nobody5390@yahoo.com>
2018-06-28 23:21:42 -0700

    debug

--- stripe_ios_imp.h
+++ stripe_ios_imp.h
@@ -1,5 +1,5 @@
 #import <Foundation/Foundation.h>
-typedef (void) (tokenfunc*) (const char *name, void *user_data);
+typedef void (tokenfunc*) (const char *name, void *user_data);
 
 @interface retToken : NSObject
 - (void) retrieveTokenObjC:(char*)myKey andcardNumber:(char*)cardNumber andexpMonth:(int)expMonth andexpYear:(int)expYear andcvc:(char*)cvc anduser_func:(tokenfunc)user_func anduser_data:(void*)user_data;
