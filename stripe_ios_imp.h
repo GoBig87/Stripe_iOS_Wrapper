7e9f463c5c325618343c8a3aeb584db28f186f1f
GoBig87 <nobody5390@yahoo.com>
2018-06-29 18:33:54 -0700

    debug

--- stripe_ios_imp.h
+++ stripe_ios_imp.h
@@ -4,3 +4,4 @@ typedef void (*tokenfunc) (const char *name, void *user_data);
 @interface retToken : NSObject
 - (void) retrieveTokenObjC:(char*)myKey andcardNumber:(char*)cardNumber andexpMonth:(int)expMonth andexpYear:(int)expYear andcvc:(char*)cvc anduser_func:(tokenfunc)user_func anduser_data:(void*)user_data;
 @end
+void retrieveToken(char* myKey, char* cardNumber, int expMonth, int expYear, char* cvc,tokenfunc user_func, void *user_data);
--- stripe_ios_imp.m
+++ stripe_ios_imp.m
@@ -44,3 +44,9 @@
 }
 
 @end
+
+void retrieveToken(char* myKey, char* cardNumber, int expMonth, int expYear, char* cvc,tokenfunc user_func, void *user_data){
+
+    retToken* retrieveToken = [[retToken alloc] init];
+    [retrieveToken retrieveTokenObjC:myKey andcardNumber:cardNumber andexpMonth:expMonth andexpYear:expYear andcvc:cvc anduser_func:user_func anduser_data:user_data];
+}
